9ec76d54781e258f27d2bb7f6366c147377dd6b0
Sinovuyo <sinovuyosolani96@gmail.com>
2020-06-09 23:41:09 -0700

    initial commit

---
+++ src/main/java/com/vaadin/intro/MainView.java
@@ -0,0 +1,29 @@
+package com.vaadin.intro;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        VerticalLayout todoList = new VerticalLayout();
+        TextField taskField = new TextField();
+        Button addButton = new Button("Add");
+        addButton.addClickShortcut(Key.ENTER);
+        addButton.addClickListener(click -> {
+            Checkbox checkBox = new Checkbox(taskField.getValue());
+            todoList.add(checkBox);
+        });
+        add(
+                new H1("My Todos"),
+                todoList,
+                new HorizontalLayout(taskField, addButton));
+    }
+}
